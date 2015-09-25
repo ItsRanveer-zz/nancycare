7e47cce049a1b2afad34a0d68266b138ada86d51
Muthukrishnan <mthukkaram@mirthcorp.com>
2015-09-26 05:07:58 +0530

    UI Mock

---
+++ www/js/controllers.js
@@ -0,0 +1,116 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PatientsCtrl', function($scope) {
+  $scope.patients = [{
+    "id":"1",
+    "name": "Armand Valencia",
+    "phone": "1-398-201-9251",
+    "picture": "41908",
+    "status" : "ion-checkmark-circled green"
+  },
+  {
+    "id":"2",
+    "name": "Chase Perez",
+    "phone": "1-257-823-6820",
+    "picture": "63324",
+    "status" : "ion-alert red"
+  },
+  {
+    "id":"3",
+    "name": "Nasim Blake",
+    "phone": "1-961-694-5994",
+    "picture": "23753",
+    "status" : "ion-checkmark-circled green"
+  },
+  {
+    "id":"4",
+    "name": "Ronan Schmidt",
+    "phone": "1-925-808-4779",
+    "picture": "10632",
+    "status" : "ion-alert red"
+  },
+  {
+    "id":"5",
+    "name": "Malcolm Hancock",
+    "phone": "1-316-162-9468",
+    "picture": "57091",
+    "status" : "ion-checkmark-circled green"
+  }];
+})
+
+.controller('PatientCtrl', function($scope, $stateParams) {
+
+  $scope.patient = {
+    "id":"5",
+    "name": "Armand Valencia",
+    "phone": "1-398-201-9251",
+    "picture": "41908",
+    "status" : "ion-checkmark-circled green"
+  };
+
+  $scope.chart = null;
+
+  $scope.showGraph = function() {
+      $scope.chart = c3.generate({
+          bindto: '#chart',
+          data: {
+            columns: [
+              ['data1', 30, 200, 100, 400, 150, 250],
+              ['data2', 50, 20, 10, 40, 15, 25]
+            ],
+            types: {
+                data1: 'area-spline',
+                data2: 'area-spline'
+            }
+          },
+          size: {
+            height: 180
+          }
+      });     
+  }
+
+
+})
+.controller('LoginCtrl', function($scope, $stateParams) {
+});
