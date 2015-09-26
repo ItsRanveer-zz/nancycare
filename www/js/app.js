75c2e91d4739d9bb09eed2c473ae344c97ab350a
Muthukrishnan <mthukkaram@mirthcorp.com>
2015-09-26 06:35:46 +0530

    Appointments

--- www/js/app.js
+++ www/js/app.js
@@ -32,11 +32,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+  .state('app.appointments', {
+    url: '/appointments',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/appointments.html'
       }
     }
   })
