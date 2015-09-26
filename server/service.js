9db13e766a1fcfe4be54ec8739e1f9d73ae90e1b
Bikash Sharma <bikashsharma.bks@gmail.com>
2015-09-26 12:46:41 +0530

    done with data upload

--- server/service.js
+++ server/service.js
@@ -1,8 +1,9 @@
 var db = require('./db.js');
 var restler =require('restler');
+var _ = require('underscore');
+var async = require('async');
 
 var service = function (){
-
 }
 
 service.prototype.collectObservation = function(patientId, token, cb){
@@ -13,11 +14,56 @@ service.prototype.collectObservation = function(patientId, token, cb){
 	    	"Accept":"application/json"
 	    } 
   	};
+  	var observation = [];
 
-  	restler.get("https://gateway.api.pcftest.com:9004/v1/fhir_rest/Observation?subject:_id="+ patientId, args)
-  	.on('complete', function(data) {
-  		data = JSON.parse(data);
-        cb(null, data.entry);
+  	var patientObservationUrl = "https://gateway.api.pcftest.com:9004/v1/fhir_rest/Observation?subject:_id=" + patientId;
+  	getData(patientObservationUrl, args, function (err, data){
+  		
+  		cb(null,data);
+
+  		getPagedData(args, data, observation, function (action, data, cb) {
+  			if (action === 'more') {
+  				console.log(data);
+  				getVitals(patientId, data);
+  				getPagedData(args, data, observation, cb);
+  			} else {
+  				observation = _.union(observation, data.entry);
+  			}
+  		});
+  	}); 	
+}
+
+
+function getPagedData (args, data, observation, cb) {
+	var nextUrl = _.find(data.link, function(l){ 
+		return l.rel == 'next'; 
+	});
+
+	if (nextUrl) {
+		getData(nextUrl.href, args, function (err, data) {
+			observation = _.union(observation, data.entry);
+			cb('more', data, cb);
+		});
+	} else {
+		cb('done');
+	}
+}
+
+function getData (url, args, cb) {
+	restler.get(url, args).on('complete', function(data) {
+  		var json = JSON.parse(data);
+        cb(null, json);
+	});
+}
+
+
+function getVitals (patientId, data) {
+	async.each(data.entry, function (entry, cb) {
+		var obj = entry.content.valueQuantity;
+		obj.patientId = patientId;
+		obj.appliesDateTime = entry.content.appliesDateTime;
+		db.insert(entry.content.name.coding[0].display, obj);
+		cb();
 	});
 }
 
